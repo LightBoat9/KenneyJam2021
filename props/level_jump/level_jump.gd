196c9db21b15d0f0d6103e6e3118d85595283951
Luke <sloopluke@gmail.com>
2021-08-21 14:04:18 -0400

    Combine Changes

--- props/level_jump/level_jump.gd
+++ props/level_jump/level_jump.gd
@@ -1,17 +1,16 @@
 extends Area2D
 
 onready var sprite: Sprite = $Sprite
+onready var audio_click: AudioStreamPlayer = $AudioClick
 export(String, MULTILINE) var next_scene: String = 'res://scenes/level_base/LevelBase.tscn'
 
 
 func _on_body_entered(body: Node) -> void:
-	sprite.frame = 1
-	yield(get_tree().create_timer(0.5), "timeout")
-	goto_next_scene()
-	
-	
-func _on_body_exited(body: Node) -> void:
-	sprite.frame = 0
+	if sprite.frame == 0:
+		sprite.frame = 1
+		audio_click.play()
+		yield(get_tree().create_timer(0.5), "timeout")
+		goto_next_scene()
 	
 	
 func goto_next_scene() -> void:
